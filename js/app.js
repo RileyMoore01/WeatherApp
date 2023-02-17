0f5d2252943fbefbb03da78d2847e09db340b35c
RileyMoore01 <xrileyxmoorex@outlook.com>
2023-02-17 17:04:43 -0600

    added comments

--- js/app.js
+++ js/app.js
@@ -2,12 +2,12 @@ async function getWeatherByZip() {
     let weatherKey = 'fed200574f31448d3c4ef74409fc60bf';
     var zipCode = document.getElementById("zipcode").value;
 
-    if (!zipCode)
+    if (!zipCode)   // If the zip code has a falsy value
     {
         alert("Not a valid zip code");
     }
 
-    else
+    else    // If the zip code has a truthy value
     {
         const response = await fetch(
             `https://api.openweathermap.org/data/2.5/weather?zip=${zipCode},us&appid=${weatherKey}&units=imperial`
