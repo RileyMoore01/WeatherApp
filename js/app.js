9504861bbf16d17ff3177d8dd9427d81f31ba1b1
RileyMoore01 <xrileyxmoorex@outlook.com>
2023-02-17 17:03:41 -0600

    null check on zipcode

--- js/app.js
+++ js/app.js
@@ -2,10 +2,25 @@ async function getWeatherByZip() {
     let weatherKey = 'fed200574f31448d3c4ef74409fc60bf';
     var zipCode = document.getElementById("zipcode").value;
 
-    const response = await fetch(
-        `https://api.openweathermap.org/data/2.5/weather?zip=${zipCode},us&appid=${weatherKey}&units=imperial`
-    )
-    
-    const data = await response.json()
-    console.log(data)
+    if (!zipCode)
+    {
+        alert("Not a valid zip code");
+    }
+
+    else
+    {
+        const response = await fetch(
+            `https://api.openweathermap.org/data/2.5/weather?zip=${zipCode},us&appid=${weatherKey}&units=imperial`
+        )
+        
+        const data = await response.json()
+        console.log(data)
+        console.log(zipCode)
+        moveIndexPage();
+    }
+}
+
+function moveIndexPage() {
+    var container = document.getElementById("input-page");
+    container.style.transform = "translateY(-1000px)";
 }
