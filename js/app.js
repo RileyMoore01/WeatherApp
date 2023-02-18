14ec6f4681cff7d34c49318168863009f7300362
RileyMoore01 <xrileyxmoorex@outlook.com>
2023-02-17 21:47:45 -0600

    json data

--- js/app.js
+++ js/app.js
@@ -2,25 +2,25 @@ async function getWeatherByZip() {
     let weatherKey = 'fed200574f31448d3c4ef74409fc60bf';
     var zipCode = document.getElementById("zipcode").value;
 
-    if (!zipCode)   // If the zip code has a falsy value
-    {
+    if (!zipCode) {  // If the zip code has a falsy value 
         alert("Not a valid zip code");
-    }
-
-    else    // If the zip code has a truthy value
-    {
+    } else {  // If the zip code has a truthy value 
         const response = await fetch(
             `https://api.openweathermap.org/data/2.5/weather?zip=${zipCode},us&appid=${weatherKey}&units=imperial`
         )
-        
         const data = await response.json()
         console.log(data)
-        console.log(zipCode)
         moveIndexPage();
     }
 }
 
 function moveIndexPage() {
-    var container = document.getElementById("input-page");
-    container.style.transform = "translateY(-1000px)";
+    
+    // Get the elements on the page
+    var text = document.getElementById("input-page");
+    var header = document.getElementById("enter-heading");
+
+    // Start animation on the properties
+    text.style.transform = "translateX(200%)";
+    header.style.transform = "translateX(1000px)";
 }
